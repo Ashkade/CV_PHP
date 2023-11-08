d6974df1c2cfc66f49ba69779a8b363360b7aa26
Ashkade <betari.laid@gmail.com>
2023-11-08 11:00:27 +0100

    création projet cv en php

---
+++ Header/header.php
@@ -0,0 +1,15 @@
+<header>
+
+<nav>
+    <div class="menu-icon">
+
+        <h1><a href=""><span class="material-symbols-outlined">home</span></a></h1>
+        <h1><a href="">Qui suis-je</a></h1>
+        <h1><a href="">Mes compétences</a></h1>
+        <h1><a href="">Mon portfolio</a></h1>
+        <h1><a href="">Me contacter</a></h1>
+
+    </div>
+</nav>
+
+</header>
---
+++ Main/main.php
@@ -0,0 +1,7 @@
+<main>
+    
+
+<p>Je suis un développeur web et web mobile passionné, spécialisé dans la création d'applications et de sites Web innovants et conviviaux. Actuellement en formation au centre Afpa d'Orléans, j'ai acquis des compétences solides en développement front-end et back-end, ainsi qu'en conception d'interfaces utilisateur attrayantes.</p>
+<p>De part ma formation initiale de Technicien Supérieur de Support en Informatique je possède une solide base professionnelle en informatique. Fort de cinq années d'expérience dans ce domaine, j'ai eu l'opportunité de travailler sur des projets variés, ce qui m'a permis de développer mon sens de la résolution de problèmes et ma capacité à collaborer efficacement au sein d'une équipe.</p>
+
+</main>
